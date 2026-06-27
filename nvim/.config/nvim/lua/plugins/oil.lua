157dd05b9cfa6c920702237a87078ad3ce6e1470
Ludovic Koenig <ludovic.koenig@gmail.com>
2026-06-27 15:53:52 +0200

    Add oil.nvim file explorer
    Edit the filesystem as a normal buffer: open the parent directory with
    -, make changes with ordinary edits, and write to apply them. Hidden
    files are shown by default and oil replaces netrw as the directory
    handler.
    
    Free <C-h> and <C-l> so smart-splits navigation works inside oil,
    moving refresh to <C-r>, and close with q.

---
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -0,0 +1,21 @@
+--- Oil ---
+
+
+return {
+  'stevearc/oil.nvim',
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  lazy = false,
+  keys = {
+    { '-', '<cmd>Oil<cr>', desc = 'Open parent directory' },
+  },
+  opts = {
+    default_file_explorer = true,
+    view_options = { show_hidden = true },
+    keymaps = {
+      ['<C-h>'] = false,
+      ['<C-l>'] = false,
+      ['<C-r>'] = 'actions.refresh',
+      ['q'] = 'actions.close',
+    },
+  },
+}
