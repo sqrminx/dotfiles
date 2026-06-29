87132c018a72c3281b046b40b17c8fba22993a4b
Ludovic Koenig <ludovic.koenig@gmail.com>
2026-06-29 07:49:16 +0200

    Migrate treesitter to the main branch
    Move nvim-treesitter and its textobjects dependency to the main branch,
    which Neovim 0.12 supports. Install parsers via the install() API, start
    highlighting from a FileType autocmd, and drop treesitter indent so the
    built-in cindent takes over, which restores correct C indentation.
    
    Remove the markdown injections override, no longer needed now that the
    main-branch queries parse cleanly on 0.12.

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -2,45 +2,23 @@
 
 return {
   'nvim-treesitter/nvim-treesitter',
-  branch = 'master',
-  build = ':TSUpdate',
+  branch = 'main',
   lazy = false,
-  dependencies = { 'nvim-treesitter/nvim-treesitter-textobjects', branch = 'master' },
-  opts = {
-    ensure_installed = { 'bash', 'c', 'lua', 'vim', 'vimdoc', 'markdown' },
-    highlight = { enable = true },
-    indent = { enable = true },
-    incremental_selection = {
-      enable = true,
-      keymaps = {
-        init_selection = '<cr>',
-        node_incremental = '<cr>',
-        node_decremental = '<bs>',
-        scope_incremental = '<tab>',
-      },
-    },
-    textobjects = {
-      select = {
-        enable = true,
-        lookahead = true,
-        keymaps = {
-          ['af'] = '@function.outer', ['if'] = '@function.inner',
-          ['ac'] = '@class.outer', ['ic'] = '@class.inner',
-          ['ap'] = '@parameter.outer', ['ip'] = '@parameter.inner',
-          ['ai'] = '@conditional.outer', ['ii'] = '@conditional.inner',
-          ['al'] = '@loop.outer', ['il'] = '@loop.inner',
-          ['acm'] = '@comment.outer',
-        },
-      },
-      move = {
-        enable = true,
-        set_jumps = true,
-        goto_next_start = { [']f'] = '@function.outer', [']c'] = '@class.outer' },
-        goto_previous_start = { ['[f'] = '@function.outer', ['[c'] = '@class.outer' },
-      },
-    },
-  },
-  config = function(_, opts)
-    require('nvim-treesitter.configs').setup(opts)
-  end,
+  build = ':TSUpdate',
+  dependencies = { 'nvim-treesitter/nvim-treesitter-textobjects', branch = 'main' },
+  config = function ()
+    require('nvim-treesitter').install(
+      {
+        'bash', 'c', 'lua',
+        'vim', 'vimdoc',
+        'markdown', 'markdown_inline'
+      }
+    )
+
+    vim.api.nvim_create_autocmd('FileType', {
+      callback = function ()
+        if not pcall(vim.treesitter.start) then return end
+      end,
+    })
+  end
 }
